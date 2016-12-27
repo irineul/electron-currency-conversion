6a58f71208ad455715451b253490e679eef56e4d
System Administrator <root@MacBook-Pro-Irineu.local>
2016-12-26 23:29:09 -0200

    code cleaning

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({width: 800, height: 600, icon: 'img/icons/icon.icns'})
 
   // and load the index.html of the app.
   win.loadURL(url.format({
@@ -17,8 +17,6 @@ function createWindow () {
     slashes: true
   }))
 
-  // Open the DevTools.
-  win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
